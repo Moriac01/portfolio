10ef2137531025c085cadd83b0e3ca2e5431f748
Moriac01 <moriackpangam@gmail.com>
2025-03-23 13:09:09 +0100

    Deuxième modification

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,12 +1,58 @@
+import React from "react";
+import { AiOutlineTwitter, AiOutlineYoutube } from "react-icons/ai";
+
+import { MdOutlineFacebook } from "react-icons/md"
 
 
 export default function Page() {
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <h1 className="text-4xl text-black font-bold">Contats</h1>
-        </main>
-  
+      <div className="pt-25 text-black">
+        <div className="px-12 pt-20 grid gap-5 md:grid-cols-2">
+          <div className="  grid gap-5">
+
+           <div className=" grid gap-5 ">
+           <h1 className="font-serif text-3xl md:text-5xl">Contact us</h1>
+            <p>
+              If you have any questions or comments, please 
+              contact us via email or phone, or send us a 
+              message using the contact form.
+            </p>
+           </div>
+
+
+           <div>
+           <h1 className=" font-bold">Email</h1>
+            <p>
+              email@example.com
+            </p>
+           </div>
+
+           <div>
+           <h1 className=" font-bold">Phone</h1>
+            <p>
+              (555) 555-5555
+            </p>
+           </div>
+
+           <div className="flex gap-2 text-3xl">
+
+
+            <MdOutlineFacebook/>
+            <AiOutlineYoutube/>
+            <AiOutlineTwitter/>
+
+           </div>
+          </div>
+
+          {/* Partie Formulaire */}
+         <form action="">
+
+          <div className="grid grid-cols-1">
+            <label htmlFor="">Name <span className="text-zinc-400">(Required)</span> </label>
+            First Name <input type="text" placeholder="Fist Name" />
+          </div>
+         </form>
+        </div>
       </div>
     );
   }
--- src/app/Residential/page.tsx
+++ src/app/Residential/page.tsx
@@ -1,12 +1,132 @@
 "use client"
 
 import React from "react";
+import Image from "next/image";
+import Footer from "../Footer";
 
 export default function page () {
   return (
     <>
-      <div className="pt-45 text-black">
-        page
+      <div className="pt-45  text-black">
+        <div className="grid gap-5  px-12  md:grid-cols-2">
+          <div>
+            <h1>
+              Residential Design
+            </h1>
+          </div>
+          <div>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Suspendisse sit amet ex vel lectus aliquet porttitor. 
+            Proin sollicitudin vitae purus at cursus. 
+            Nullam suscipit ultricies lectus in efficitur. 
+            Phasellus hendrerit enim lectus. 
+            Nunc vitae eleifend metus. 
+            <span className="">
+            Vestibulum ante ipsum primis in faucibus orci luctus et
+             ultrices posuere cubilia Curae; Vivamus eu vehicula felis, 
+             vel congue purus. Integer ut elit feugiat sem semper mollis. 
+             Etiam malesuada eros turpis, id dignissim justo dictum et. 
+             Ut a nisl sed mauris luctus dapibus sed ut lacus. 
+             Phasellus sit amet neque tortor. Donec sit amet risus viverra, 
+             vulputate arcu et, pellentesque risus.
+            </span>
+            </p>
+          </div>
+        </div>
+
+        <div className="px-12 grid gap-2 grid-cols-2">
+          
+          
+        <div>
+            <Image
+            src="/Stocks_1.jpg"
+            alt=""
+            width={500}
+            height={200}
+            className="h-75"
+            />
+          </div>
+
+          <div>
+            <Image
+            src="/Stocks_1.jpg"
+            alt=""
+            width={500}
+            height={200}
+            className="h-75"
+            />
+          </div>
+
+          <div>
+            <Image
+            src="/Stocks_1.jpg"
+            alt=""
+            width={500}
+            height={200}
+            className="h-75"
+            />
+          </div>
+
+          <div>
+            <Image
+            src="/Stocks_1.jpg"
+            alt=""
+            width={500}
+            height={200}
+            className="h-75"
+            />
+          </div>
+        </div>
+
+        <div className="pt-10 px-12 grid gap-4">
+          <div>
+            <h4>
+              Credits
+            </h4>
+          </div>
+          <div>
+            <p>
+              Firsname Lastname
+            </p>
+            <p>
+              Title/Role
+            </p>
+          </div>
+
+          <div>
+            <p>
+             Firstname Lastname 
+            </p>
+            <p>
+              Title/Role
+            </p>
+          </div>
+
+          <div>
+            <p>
+             Firstname Lastname 
+            </p>
+            <p>
+              Title/Role
+            </p>
+          </div>
+
+          <div>
+            <p>
+             Firstname Lastname 
+            </p>
+            <p>
+              Title/Role
+            </p>
+          </div>
+
+          
+        </div>
+      </div>
+
+      <div>
+        <Footer/>
       </div>
     </>
     
